0e27264546dec2c95658984db597c4dfdbdeca98
Skydogzz <tstephan00@outlook.com>
2026-03-24 15:36:22 +0100

    initial commit

---
+++ lua/plugins/core.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = true,
+      },
+    },
+  },
+  {
+    "lazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
