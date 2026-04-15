32ea9db23ed1e0402b0535b017f2ff41148cb704
Skydogzz <tstephan00@outlook.com>
2026-04-15 20:38:10 +0200

    fix config

---
+++ lua/plugins/cpp.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {},
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "clangd" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "c", "cpp" })
+    end,
+  },
+}
--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -11,10 +11,6 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      format_on_save = {
-        timeout_ms = 1000,
-        lsp_fallback = true,
-      },
       formatters_by_ft = {
         javascript = { "prettierd", "prettier" },
         javascriptreact = { "prettierd", "prettier" },
